986471e2d7a5f73a93df255883c3c09ca01e19be
David Siqueira <davidsiqueira.hog@gmail.com>
2023-02-12 10:53:41 -0300

    responsividade cards

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -38,82 +38,97 @@ const Dashboard = () => {
           {/* Cards */}
           <Flex
             minH="270px"
-            flexDirection={["column", "column", "column", "row", "row"]}
-            justifyContent="space-between"
+            flexDirection={["column", "column", "column", "column", "row"]}
+            rowGap="25px"
+            columnGap="25px"
+            alignSelf="center"
           >
-            <DashboardCard
-              mediaUltimos30Dias={600}
-              totalHoje={660}
-              accontName="Todas as contas"
-              cardColor="#C361FF"
-              infoGrafico={{
-                series: [
-                  {
-                    name: "teste",
-                    data: [
-                      110, 100, 200, 250, 300, 324, 342, 342, 342, 342, 215,
-                      334, 110, 100, 200, 250, 300, 215, 334, 324, 342, 342,
-                      342, 342, 234, 324, 234, 324, 434, 434,
-                    ],
-                  },
-                ],
-              }}
-            />
-            <DashboardCard
-              mediaUltimos30Dias={300}
-              totalHoje={250}
-              accontName="Cash"
-              cardColor="#7B61FF"
-              infoGrafico={{
-                series: [
-                  {
-                    name: "teste",
-                    data: [
-                      110, 334, 324, 342, 342, 200, 250, 300, 215, 334, 324,
-                      342, 342, 342, 342, 234, 324, 434, 110, 100, 300, 215,
-                      342, 234, 324, 342, 100, 200, 250, 434,
-                    ],
-                  },
-                ],
-              }}
-            />
-            <DashboardCard
-              mediaUltimos30Dias={200}
-              totalHoje={60}
-              accontName="Pix"
-              cardColor="#007AFF"
-              infoGrafico={{
-                series: [
-                  {
-                    name: "teste",
-                    data: [
-                      342, 234, 342, 342, 342, 100, 342, 342, 342, 342, 234,
-                      200, 250, 300, 215, 334, 324, 324, 434, 110, 110, 100,
-                      200, 250, 300, 215, 334, 324, 324, 434,
-                    ],
-                  },
-                ],
-              }}
-            />
-            <DashboardCard
-              mediaUltimos30Dias={400}
-              totalHoje={300}
-              accontName="Cartão"
-              cardColor="#00A62E"
-              infoGrafico={{
-                series: [
-                  {
-                    name: "teste",
-                    data: [
-                      250, 250, 300, 215, 334, 300, 110, 100, 200, 324, 434,
-                      324, 342, 342, 342, 342, 234, 110, 342, 342, 234, 100,
-                      200, 215, 334, 324, 342, 342, 324, 434,
-                    ],
-                  },
-                ],
-              }}
-            />
+            <Flex
+              rowGap="25px"
+              columnGap="25px"
+              flexDirection={["column", "column", "row", "row", "row"]}
+            >
+              <DashboardCard
+                mediaUltimos30Dias={600}
+                totalHoje={660}
+                accontName="Todas as contas"
+                cardColor="#C361FF"
+                infoGrafico={{
+                  series: [
+                    {
+                      name: "teste",
+                      data: [
+                        110, 100, 200, 250, 300, 324, 342, 342, 342, 342, 215,
+                        334, 110, 100, 200, 250, 300, 215, 334, 324, 342, 342,
+                        342, 342, 234, 324, 234, 324, 434, 434,
+                      ],
+                    },
+                  ],
+                }}
+              />
+              <DashboardCard
+                mediaUltimos30Dias={300}
+                totalHoje={250}
+                accontName="Cash"
+                cardColor="#7B61FF"
+                infoGrafico={{
+                  series: [
+                    {
+                      name: "teste",
+                      data: [
+                        110, 334, 324, 342, 342, 200, 250, 300, 215, 334, 324,
+                        342, 342, 342, 342, 234, 324, 434, 110, 100, 300, 215,
+                        342, 234, 324, 342, 100, 200, 250, 434,
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </Flex>
+            <Flex
+              rowGap="25px"
+              columnGap="25px"
+              flexDirection={["column", "column", "row", "row", "row"]}
+            >
+              <DashboardCard
+                mediaUltimos30Dias={200}
+                totalHoje={60}
+                accontName="Pix"
+                cardColor="#007AFF"
+                infoGrafico={{
+                  series: [
+                    {
+                      name: "teste",
+                      data: [
+                        342, 234, 342, 342, 342, 100, 342, 342, 342, 342, 234,
+                        200, 250, 300, 215, 334, 324, 324, 434, 110, 110, 100,
+                        200, 250, 300, 215, 334, 324, 324, 434,
+                      ],
+                    },
+                  ],
+                }}
+              />
+              <DashboardCard
+                mediaUltimos30Dias={400}
+                totalHoje={300}
+                accontName="Cartão"
+                cardColor="#00A62E"
+                infoGrafico={{
+                  series: [
+                    {
+                      name: "teste",
+                      data: [
+                        250, 250, 300, 215, 334, 300, 110, 100, 200, 324, 434,
+                        324, 342, 342, 342, 342, 234, 110, 342, 342, 234, 100,
+                        200, 215, 334, 324, 342, 342, 324, 434,
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </Flex>
           </Flex>
+          <Flex></Flex>
         </Box>
       </Flex>
     </SidebarWithHeader>
