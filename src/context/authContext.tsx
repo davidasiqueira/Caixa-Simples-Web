b1bbb53ee04bd8c6996d4cbf45762b8f349b4ece
David Siqueira <davidsiqueira.hog@gmail.com>
2023-02-14 22:49:30 -0300

    AJUSTES PARA AUTH

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -67,7 +67,7 @@ export function AuthProvider({ children }) {
       setCookie(undefined, "caixa-simples-token", access_token, {
         maxAge: 300, // 5 minutes
       });
-      setCookie(undefined, "caixa-simples-userId", user.userId, {
+      setCookie(undefined, "caixa-simples-userId", user.id, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
       });
 
