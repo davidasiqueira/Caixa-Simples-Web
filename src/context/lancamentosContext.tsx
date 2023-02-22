b4122776888975873bf05dd35a1682ef0f1a3c78
David Siqueira <davidsiqueira.hog@gmail.com>
2023-02-21 22:39:58 -0300

    inicio lancamento context

---
+++ src/context/lancamentosContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+type LancamentoType = {
+  movimento: string;
+  valor: number;
+  conta: string;
+  descricao: string;
+  hora: number;
+};
+
+type LancamentosContextType = {
+  lancamentos: LancamentoType[];
+  lancamentosSync: () => Promise<void>;
+  addLancamento: (lancamento: LancamentoType) => Promise<void>;
+};
+
+export const LancamentosContext = createContext({} as LancamentosContextType);
+
+export function LancamentosProvider({ children }) {
+  const [lancamentos, setLancamento] = useState<LancamentoType[]>([]);
+
+  async function addLancamento(lancamento: LancamentoType) {
+    setLancamento((prevState) => {
+      [...prevState, lancamento];
+    });
+  }
+
+  async function lancamentosSync() {}
+  return (
+    <LancamentosContext.Provider
+      value={{ addLancamento, lancamentosSync, lancamentos }}
+    >
+      {children}
+    </LancamentosContext.Provider>
+  );
+}
